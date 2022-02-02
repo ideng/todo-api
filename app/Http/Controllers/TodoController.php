848d87911109754de69454ca1c69f237f406d751
ideng.dewoadit@gmail.com <ideng.dewoadit@gmail.com>
2022-02-02 14:31:41 +0700

    feat: add todo endpoints

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use App\Http\Requests\StoreTodoRequest;
+use App\Http\Requests\UpdateTodoRequest;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(
+            Todo::all()
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreTodoRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreTodoRequest $request)
+    {
+        return response()->json(
+            Todo::create($request->validated()),
+            201
+        );
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Todo $todo)
+    {
+        return response()->json(
+            $todo
+        );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateTodoRequest  $request
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateTodoRequest $request, Todo $todo)
+    {
+        $todo->update($request->validated());
+
+        return response()->json(
+            Todo::find($todo->id)
+        );
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Todo $todo)
+    {
+        $todo->delete();
+
+        return response()->json(
+            $todo
+        );
+    }
+}
